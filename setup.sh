4a8b137aa6bb1237a9a25faafbddb4fe251ffbff
sostema <sostema@gmail.com>
2020-09-29 09:20:00 +0300

    setup fixes

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ rm -f /Users/sostema/.p10k.zsh
 rm -f /Users/sostema/.config/nvim
 rm -f /Users/sostema/.config/vifm
 
-# Create symlinks for config dotfiles
-ln -s /Users/sostema/.dotfiles/.aliases /Users/sostema
+# Create symlinks for configdotfiles
+ln -s /Users/sostema/dotfiles/.aliases /Users/sostema
 mkdir /Users/sostema/.config
-ln -s /Users/sostema/.dotfiles/.config/nvim /Users/sostema/.config
-ln -s /Users/sostema/.dotfiles/.config/vifm /Users/sostema/.config
-ln -s /Users/sostema/.dotfiles/.config/zsh/.p10k.zsh /Users/sostema
-ln -s /Users/sostema/.dotfiles/.config/zsh/.tmux.conf /Users/sostema
-ln -s /Users/sostema/.dotfiles/.config/zsh/.zshrc /Users/sostema
+ln -s /Users/sostema/dotfiles/.config/nvim /Users/sostema/.config
+ln -s /Users/sostema/dotfiles/.config/vifm /Users/sostema/.config
+ln -s /Users/sostema/dotfiles/.config/zsh/.p10k.zsh /Users/sostema
+ln -s /Users/sostema/dotfiles/.config/zsh/.tmux.conf /Users/sostema
+ln -s /Users/sostema/dotfiles/.config/zsh/.zshrc /Users/sostema
